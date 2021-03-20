d605f698fa4a4936efd623323bfbf21e77748be8
Joseph Guerrero <joseph.guerrero1@outlook.com>
2021-03-20 10:53:39 -0600

    Fixed syntax typos

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 // Variables on user age and whether they are subscribed or not. userAge is an integer and is_subscribed is a boolean.
-var userAge = 18;
-var is_subscribed = "True";
+var userAge = 19;
+var is_subscribed = true;
 
 // if, else if, else conditional block with four possible combinations of the following scenarios: Whether the user is younger than 18 or not, and whether the user is subscribed or not. 
-if (userAge < 18, is_subscribed = "False") {
+if (userAge < 18, is_subscribed = false) {
     console.log("The user is younger than 18 and not subscribed");
-} else if (userAge < 18, is_subscribed = "True") {
+} else if (userAge < 18, is_subscribed = true) {
     console.log("The user is younger than 18 and is subscribed");
-} else if (userAge >= 18, is_subscribed = "False") {
+} else if (userAge >= 18, is_subscribed = false) {
     console.log("The user is 18 or older and not subscribed");
-} else if (userAge >= 18, is_subscribed = "True") {
+} else if (userAge >= 18, is_subscribed = true) {
     console.log("The user is 18 or older and is subscribed");
 } else {
     console.log("Invalid");
