3b193b7200e8e52a39aa7662d2ac45eb222a2c85
Joseph Guerrero <joseph.guerrero1@outlook.com>
2021-03-20 16:28:31 -0600

    Fixed mistakes

--- app.js
+++ app.js
@@ -3,13 +3,13 @@ var userAge = 19;
 var is_subscribed = true;
 
 // if, else if, else conditional block with four possible combinations of the following scenarios: Whether the user is younger than 18 or not, and whether the user is subscribed or not. 
-if (userAge < 18, is_subscribed = false) {
+if (userAge < 18 && is_subscribed === false) {
     console.log("The user is younger than 18 and not subscribed");
-} else if (userAge < 18, is_subscribed = true) {
+} else if (userAge < 18 && is_subscribed === true) {
     console.log("The user is younger than 18 and is subscribed");
-} else if (userAge >= 18, is_subscribed = false) {
+} else if (userAge >= 18 && is_subscribed === false) {
     console.log("The user is 18 or older and not subscribed");
-} else if (userAge >= 18, is_subscribed = true) {
+} else if (userAge >= 18 && is_subscribed === true) {
     console.log("The user is 18 or older and is subscribed");
 } else {
     console.log("Invalid");
